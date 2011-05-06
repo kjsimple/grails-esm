fbbac760e4b4e4fe9c345f4cc8f26e19f7882488
kjsimple <kjsimple@gmail.com>
2011-05-06 17:55:57 +0800

    menus

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,7 +8,7 @@ class BootStrap {
             new Purpose(name: "\u4f1a\u8bae", description: ""),
             new Purpose(name: "\u4e1a\u52a1\u5f00\u53d1", description: ""),
             new EsmRole(name: 'admin', description: 'Administration Role'),
-            new EsmRole(name: 'user', description: 'Normal user').save()
+            new EsmRole(name: 'user', description: 'Normal user')
         ].each {it.save(flush: true)}
 
         def user = new EsmUser(userID: 'david', firstName: 'xj', lastName: 'Guo', password: 'm')
@@ -17,8 +17,20 @@ class BootStrap {
 
         user = new EsmUser(userID: 'admin', firstName: 'admin', lastName: 'admin', password: 'm')
         user.addToRoles(EsmRole.findByName('admin'))
-        user.save()
+        user.save(flush: true)
+
+        def rootModule = new Module(name: 'ROOT')
+        def homeModule = new Module(name: 'Home', action: '/')
+        def logoutModule = new Module(name: 'Logout', action: '/securityController')
+        homeModule.parent = rootModule
+        def adminModule = new Module(name: "admin")
+        rootModule.addToChildren(homeModule)
+        rootModule.addToChildren(adminModule)
+        rootModule.addToChildren(logoutModule)
+        rootModule.save(flush: true)
     }
+    
     def destroy = {
     }
+    
 }
--- grails-app/domain/com/gydoc/esm/domain/Module.groovy
+++ grails-app/domain/com/gydoc/esm/domain/Module.groovy
@@ -3,9 +3,13 @@ package com.gydoc.esm.domain
 class Module {
 
     String name
+    String action
+    Module parent
+    static hasMany = [children: Module]
 
     static constraints = {
         name(blank: false, nullable: false, unique: true, maxSize: 100)
+        action(blank: true, nullable: true, maxSize: 500)
     }
 
 }
